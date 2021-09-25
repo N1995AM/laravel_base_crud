1a6890970ee03249a72f9350a0972404879ca502
unknown <andreiv0202@gmail.com>
2021-09-26 01:59:16 +0900

    tailwind style on create

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
 
-<h1>Products Create</h1>
 <hr/>
+
 
 @if ($errors->any())
 <div class="alert alert-danger">
@@ -15,23 +15,25 @@
 </div>
 @endif
 
-<form action="{{ route('products.store') }}" method="post">
-    @csrf
-    <input type="text" name="name" class="form-control mb-3" placeholder="Product Name"/>
-    
-    <input type="number" name="price" class="form-control mb-3" placeholder="Price $$"/>
-    
-    <textarea class="form-control mb-3" name="description" rows="4" placeholder="Description"></textarea>
-    
- 
 
+<form action="{{ route('products.store') }}" method="post" >
+    @csrf
+
+    <div class="flex justify-center">
+        
+        <div class="w-5/6 my-5">
+            <p class="text-3xl font-sans tracking-tighter">Products Create</p>
+
+            <input type="text" name="name" class="focus:border-light-blue-500 focus:ring-1 focus:ring-light-blue-500 focus:outline-none w-full text-sm text-black placeholder-gray-500 border border-gray-200 rounded-md py-2 pl-10 p-4 my-3" placeholder="Product Name"/>
+            <input type="number" name="price" class="focus:border-light-blue-500 focus:ring-1 focus:ring-light-blue-500 focus:outline-none w-full text-sm text-black placeholder-gray-500 border border-gray-200 rounded-md py-2 pl-10 p-4 my-3" placeholder="Price $$"/>   
+            <textarea class="focus:border-light-blue-500 focus:ring-1 focus:ring-light-blue-500 focus:outline-none w-full text-sm text-black placeholder-gray-500 border border-gray-200 rounded-md py-2  p-4 my-3" name="description" rows="4" placeholder="Description"></textarea>
+        </div> 
+    </div>
     <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
         <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
           Save
         </button>
     </div>
-
-
 </form>
 
 
